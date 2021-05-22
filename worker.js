fbc3351ce28ef42d823ced224847d886babb3f60
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-05-21 19:36:59 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,17 @@
 const { Worker } = require("discord-rose");
 const worker = new Worker();
-let fs = require('fs')
+let fs = require("fs");
 
 worker.commands.prefix(",,");
-let commands = fs.readdirSync("./commands/")
+let commands = fs.readdirSync("./commands/");
 for (let command of commands) {
-    let currentCommand = require(`./commands/${command}`)
-    worker.commands.add({
-        command: currentCommand.config.name,
-        aliases: currentCommand.config.aliases,
-        exec: currentCommand.exec
-    })
+    let currentFolder = fs.readdirSync(`./commands/${command}`);
+    for (let folder of currentFolder) {
+        let currentCommand = require(`./commands/${command}/${folder}`);
+        worker.commands.add({
+            command: currentCommand.config.name,
+            aliases: currentCommand.config.aliases,
+            exec: currentCommand.exec
+        });
+    }
 }
